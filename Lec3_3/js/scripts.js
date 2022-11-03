4ef4f2878fe095aa2afa28b62b93f903c85a9352
Melan1408 <andreymelnik9517@gmail.com>
2022-11-03 21:39:33 +0200

    Homework 3 part 3 was added

---
+++ Lec3_3/js/scripts.js
@@ -0,0 +1,15 @@
+let firstNumber = parseFloat(prompt(`Input please first number`));
+let secondNumber = parseFloat(prompt(`Input please second number`));
+let thirdNumber =  parseFloat(prompt(`Input please third number`));
+
+let arithmeticMean = (firstNumber + secondNumber + thirdNumber)/3;
+
+if(isNaN(arithmeticMean))
+{
+    alert(`You input numbers was incorrect, please use only numbers.`);
+}
+else
+{
+    alert(`The arithmetic mean of this numbers is ${arithmeticMean}`);
+}
+
