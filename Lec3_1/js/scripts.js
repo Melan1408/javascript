7e97d4aa287735655710d3a65e05dc5961c9ee9d
Melan1408 <andreymelnik9517@gmail.com>
2022-11-03 21:09:49 +0200

    Homework 3 part 1 was added

---
+++ Lec3_1/js/scripts.js
@@ -0,0 +1,39 @@
+let userPoints=0;
+
+userAnswer = prompt(`How many corners does a rectangle have?`);
+
+if (parseInt(userAnswer)===4)
+{
+    userPoints +=10;
+}
+
+userAnswer = prompt(`Is the sun a star or planet?`);
+
+if (userAnswer && userAnswer.trim().toLowerCase()==="star")
+{
+    userPoints +=10;
+}
+
+userAnswer = prompt(`What year was Kiev founded?`);
+
+if (parseInt(userAnswer)===882)
+{
+    userPoints +=10;
+}
+
+userAnswer = prompt(`What is the largest planet in the solar system`);
+
+if (userAnswer && userAnswer.trim().toLowerCase()==="jupiter")
+{
+    userPoints +=10;
+}
+
+userAnswer = prompt(`Is Beijing capital city of China?`);
+
+if (userAnswer && userAnswer.trim().toLowerCase()==="yes")
+{
+    userPoints +=10;
+}
+
+alert (`You have ${userPoints} points. You answered correctly for ${userPoints/10}/5.`);
+
