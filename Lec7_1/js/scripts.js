71af72e465573607fcce45107002fe64e1c61fd4
Melan1408 <andreymelnik9517@gmail.com>
2022-11-23 01:11:30 +0200

    Homework 7_1 is done

--- Lec7_1/js/scripts.js
+++ Lec7_1/js/scripts.js
@@ -1,13 +1,49 @@
-const array = [123, 244, "hello", 13, "cat", 6];
+let htmlViewArray = [];
+let suitPathArray = ['clubs', 'spades', 'diamonds', 'hearts'];
+let personCardPathArray = ['jack', 'queen', 'king'];
+let personCardNameArray = ['J', 'Q', 'K', 'T'];
 
-function removeElement(array, item) {
-    index = array.indexOf(item);
-    if (index >= 0) array.splice(index, 1);
+htmlViewArray.push(`<div class="wrapper">`);
+
+for (let i = 2; i <= 10; i++) {
+    addNumberCard(htmlViewArray, i);
 }
 
-removeElement(array,"cat");
-console.log(array);
+for (let i = 0; i < 4; i++) {
+    addPersonCard(htmlViewArray, personCardNameArray[i], personCardPathArray[i]);
+}
 
+function addNumberCard(cardsArray, numberCardName) {
+    for (let i = 0; i < 4; i++) cardsArray.push(`
+        <div class="card">
+            <div class="card__info">${numberCardName}
+                <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+            </div>
+                <div class="card__info">${numberCardName}
+            <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+             </div>
+        </div>
+        `);
+}
 
+function addPersonCard(cardsArray, personCardName, personCardPath) {
+    for (let i = 0; i < 4; i++) {
+        if (personCardName === 'T') personCardPath = suitPathArray[i];
+        cardsArray.push(`
+		<div class="card card--person">
+			<div class="card__info">${personCardName}
+			    <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+			</div>
+			<img class="person" src="images/${personCardPath}.svg" alt=${suitPathArray[i]}>
+			<div class="card__info">${personCardName}
+			    <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+			</div>
+		</div>
+        `);
+    }
+}
 
+htmlViewArray.push(`</div>`);
 
+let htmlView = htmlViewArray.join(``);
+document.write(htmlView);
