90efe22164a0677f6b18af6bb8598ff5a15aece2
Melan1408 <andreymelnik9517@gmail.com>
2022-11-23 10:08:36 +0200

    Homework 7_1 is done

--- Lec7_1/js/scripts.js
+++ Lec7_1/js/scripts.js
@@ -6,37 +6,37 @@ let personCardNameArray = ['J', 'Q', 'K', 'T'];
 htmlViewArray.push(`<div class="wrapper">`);
 
 for (let i = 2; i <= 10; i++) {
-    addNumberCard(htmlViewArray, i);
+    addNumberCard(htmlViewArray, i, suitPathArray);
 }
 
 for (let i = 0; i < 4; i++) {
-    addPersonCard(htmlViewArray, personCardNameArray[i], personCardPathArray[i]);
+    addPersonCard(htmlViewArray, personCardNameArray[i], personCardPathArray[i], suitPathArray);
 }
 
-function addNumberCard(cardsArray, numberCardName) {
+function addNumberCard(cardsArray, numberCardName, suitPath) {
     for (let i = 0; i < 4; i++) cardsArray.push(`
         <div class="card">
             <div class="card__info">${numberCardName}
-                <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+                <img src="images/${suitPath[i]}.svg" alt=${suitPath[i]}>
             </div>
                 <div class="card__info">${numberCardName}
-            <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+            <img src="images/${suitPath[i]}.svg" alt=${suitPath[i]}>
              </div>
         </div>
         `);
 }
 
-function addPersonCard(cardsArray, personCardName, personCardPath) {
+function addPersonCard(cardsArray, personCardName, personCardPath, suitPath) {
     for (let i = 0; i < 4; i++) {
-        if (personCardName === 'T') personCardPath = suitPathArray[i];
+        if (personCardName === 'T') personCardPath = suitPath[i];
         cardsArray.push(`
 		<div class="card card--person">
 			<div class="card__info">${personCardName}
-			    <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+			    <img src="images/${suitPath[i]}.svg" alt=${suitPath[i]}>
 			</div>
-			<img class="person" src="images/${personCardPath}.svg" alt=${suitPathArray[i]}>
+			<img class="person" src="images/${personCardPath}.svg" alt=${suitPath[i]}>
 			<div class="card__info">${personCardName}
-			    <img src="images/${suitPathArray[i]}.svg" alt=${suitPathArray[i]}>
+			    <img src="images/${suitPath[i]}.svg" alt=${suitPath[i]}>
 			</div>
 		</div>
         `);
