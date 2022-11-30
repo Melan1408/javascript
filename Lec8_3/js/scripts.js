26ac6c702ae7a174fb1c9c6dd15ddbbdd6a5bed7
Melan1408 <andreymelnik9517@gmail.com>
2022-11-30 16:18:50 +0200

    Homework 8 is done

--- Lec8_3/js/scripts.js
+++ Lec8_3/js/scripts.js
@@ -1,3 +1,7 @@
+console.log(main(2, 5, pow));
+console.log(main(2, 5));
+console.log(main());
+
 function sum (a, b) {
     return a + b;
 }
@@ -8,4 +12,8 @@ function main (a = 2, b = 3, c) {
         return c(sumResult);
     }
     return sumResult;
+}
+
+function pow (a) {
+    return a * a;
 }
