37379ba2fad98091af668e7fa8028067de8c7210
Melan1408 <andreymelnik9517@gmail.com>
2022-12-20 22:48:59 +0200

    Homework 12.1 is done

--- Lec12_1/js/scripts.js
+++ Lec12_1/js/scripts.js
@@ -1,50 +1,38 @@
-let marvelHeroes = [
-    {
-        name: "Thor"
-    },
-    {
-        name: "Spider Man"
-    }
-];
+function Human(name, age) {
+    this.name = name;
+    this.age = age;
 
-let dcHeroes = [
-    {
-        name: "Superman"
-    },
-    {
-        name: "Batman"
-    },
-    {
-        name: "Deadpool"
+    this.getInfo = function() {
+        console.log(`Name is ${this.name} and age is ${this.age}.`);
     }
-];
+}
 
-Array.prototype.heroesRender = function(path) {
-    const tableRender = this
-        .map(function(item) {
-            return `
-                <tr>
-                    <td>${item.name}</td>
-                    <td>
-                        <img src="images/${path}/${item.name.toLowerCase().replaceAll(" ","")}.svg" width="50" height="50">
-                    </td>
-                </tr>
-            `;
-        })
-        .join("");
-    document.write(`
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Icon</th>
-                </tr>
-            </thead>
-            <tbody>
-                ${tableRender}
-            </tbody>
-        </table>
-    `);
-};
-marvelHeroes.heroesRender("marvel");
-dcHeroes.heroesRender("dc");
+function Car(mark, model, yearOfProduct, numberSign) {
+    this.mark = mark;
+    this.model = model;
+    this.yearOfProduct = yearOfProduct;
+    this.numberSign = numberSign;
+
+    this.getOwner = function(owner) {
+        if(owner.age > 18)  this.owner = owner;
+        else console.log("Owner age must be more than 18 year.");
+    }
+
+    this.getInfo = function() {
+        console.log(`${this.mark} ${this.model} with year of product ${this.yearOfProduct} and number sign ${this.numberSign}.`);
+        if (this.owner !== undefined)  this.owner.getInfo();
+        else console.log ("There is no owner of car.");
+    }
+}
+
+const personFirst = new Human("Andrey", 27);
+const personSecond = new Human("Ivan", 17);
+
+const carFirst = new Car("Mazda", "6", 2014, "AE0912AO");
+const carSecond = new Car("Nissan", "Sentra", 2007, "АН2413КЕ");
+
+carFirst.getOwner(personFirst);
+carSecond.getOwner(personSecond);
+
+carFirst.getInfo();
+carSecond.getInfo();
