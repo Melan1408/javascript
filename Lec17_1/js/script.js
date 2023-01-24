d767a27e5956f7b9f395c0f929a1faaca799e1c6
Melan1408 <andreymelnik9517@gmail.com>
2023-01-24 23:18:53 +0200

    Homework 17.1 is done

---
+++ Lec17_1/js/script.js
@@ -0,0 +1,30 @@
+const emojiArray = ['🤑', '🤠', '🤡', '😡', '🥶'];
+
+class Emoji{
+    constructor(score, picture) {
+        this.score = score;
+        this.picture = picture;
+    }
+
+    render() {
+        const container = document.createElement('div');
+        const pictureTag = document.createElement('p');
+        const scoreTag = document.createElement('p');
+
+        pictureTag.innerText = this.picture;
+        scoreTag.innerText = this.score;
+
+        pictureTag.addEventListener('click', () => {
+            scoreTag.innerText++;
+        })
+
+        container.append(pictureTag);
+        container.append(scoreTag);
+        document.body.append(container);
+    }
+}
+
+emojiArray.forEach(item => {
+    const emoji = new Emoji(0, item);
+    emoji.render();
+})
