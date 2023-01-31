103493352beee5004d2d291ff520fe86c2af03df
Melan1408 <andreymelnik9517@gmail.com>
2023-01-31 22:47:25 +0200

    Homework 20.1 is done

---
+++ Lec20_1/js/script.js
@@ -0,0 +1,26 @@
+const convertedData = [];
+let counter = 0;
+
+function getDataJson(method, actions, array, counter) {
+
+    const xml = new XMLHttpRequest();
+    xml.open(method, actions[counter]);
+    xml.send();
+
+    xml.responseType = 'json';
+    xml.onload = () => {
+        if (xml.status != 200) {
+            alert(`Ошибка ${xml.status}: ${xml.statusText}`);
+            counter++;
+        }
+        else {
+            const response= xml.response.children;
+            array.push(response);
+            counter++;
+        }
+        if(counter < actions.length) getDataJson(method, actions, array, counter);
+        else console.log(`${array}`);
+    };
+}
+
+getDataJson('GET', ['data.json', 'data2.json'], convertedData, counter);
