317ac815cc198b54192757f477fb8d0ad394c62a
Melan1408 <andreymelnik9517@gmail.com>
2023-03-01 17:27:33 +0200

    Homework 27.1 is done

---
+++ Lec27_1/js/calculator/calculator.js
@@ -0,0 +1,6 @@
+import add from './add.js';
+import sub from './sub.js';
+import div from './div.js';
+import mul from './mul.js';
+
+export const calculator = { add, sub, div, mul };
---
+++ Lec27_1/js/script.js
@@ -0,0 +1,6 @@
+import { calculator } from "./calculator/calculator.js";
+
+console.log(calculator.add(13, 5));
+console.log(calculator.sub(22, 33));
+console.log(calculator.mul(2, 11));
+console.log(calculator.div(15, 3));
